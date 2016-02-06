8542ca51f3f9a39e1ea818e489ee8acb2bf6195f
Brandon Stookey <brandonstookey@Brandons-MacBook-Pro.local>
2016-02-05 20:27:02 -0600

    Built Table

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,63 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+DROP TABLE IF EXISTS `userTable`;
+		
+CREATE TABLE `userTable` (
+  `id` INTEGER(20) AUTO_INCREMENT NOT NULL,
+  `username` VARCHAR(15),
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'messageTable'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS `messageTable`;
+		
+CREATE TABLE `messageTable` (
+  `id` INTEGER(20) AUTO_INCREMENT NOT NULL,
+  `messages` VARCHAR(50),
+  PRIMARY KEY (`id`)
+);
 
+-- ---
+-- Table 'roomTable'
+-- 
+-- ---
 
+DROP TABLE IF EXISTS `roomTable`;
+		
+CREATE TABLE `roomTable` (
+  `id` INTEGER(20) AUTO_INCREMENT NOT NULL,
+  `roomname` VARCHAR(20),
+  PRIMARY KEY (`id`)
+);
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+-- ---
+-- Foreign Keys 
+-- ---
 
+ALTER TABLE `messageTable` ADD FOREIGN KEY (id) REFERENCES `userTable` (`id`);
+ALTER TABLE `messageTable` ADD FOREIGN KEY (id) REFERENCES `roomTable` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `userTable` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messageTable` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `roomTable` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `userTable` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `messageTable` (`id`,`messages`) VALUES
+-- ('','');
+-- INSERT INTO `roomTable` (`id`,`roomname`) VALUES
+-- ('','');
